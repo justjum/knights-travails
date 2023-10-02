8f78154a7485992254ab9f6c72b04864edb9c8bc
justjum <jumasaurusrex@gmail.com>
2023-10-02 20:13:57 +1030

    Used "map" to track path taken - Implemented my newly discovered Map object - Used this to map out the route being taken as the nodes were being added to the queue, then looking up that path to append when that node's adjacentcies were being added. - This one really hurt my brain, but I've learnt a lot:
    Key learnings:
    - Map objects are great for storing + adding to complex key value pairs
    - Recursive functions are not always the best option

--- script.js
+++ script.js
@@ -1,67 +1,52 @@
-
-class board {
+class gameBoard {
     constructor() {
-        this.nodesList = buildBoard(8,8);
         this.adjacencyList = new Map();
     }
 
-    setNodes() {
-        //console.log(this.nodesList);
-        for (let x=0; x<this.nodesList.length; x++) {
-            //console.log(`setting node ${x}`)
-            this.nodesList[x] = new node(this.nodesList[x], this.nodesList);
-            
-        }
+    addVertex(v) {
+        this.adjacencyList.set(v,[]);
     }
 
-    addVertex(nodesList) {
-       // for (i in nodesList) {
-            this.adjacencyList.set(nodesList, []);
-            
-        //}
-        
+    addEdges(v,w) {
+        this.adjacencyList.get(v.toString()).push(w.toString());
+        this.adjacencyList.get(w.toString()).push(v.toString());
     }
 
-    addEdges(v, w) {
-        this.adjacencyList(v).push(w);
-        this.adjacencyList(w).push(v);
-        console.log(this.adjacencyList);
-    }
-
-    moveKnight(adjList = this.nodesList, startPosition, endPosition, moveCounter=0, moves=[]) {
-        if (startPosition[0] === endPosition[0] && startPosition[1] === endPosition[1]) {
-            return console.log('Same position, no moves');
-        }
-        else {
-            let adjacent = this.getAdjacent(adjList, startPosition);
-            return knightMoves(adjacent, startPosition, endPosition, moveCounter, moves)
-            
-        }
-        
-    }
-
-    getAdjacent(nodesList, position) {
-        for (i in nodesList) {
-            if ((nodesList[i].position[0] === position[0] && nodesList[i].position[1] === position[1] )) {
-                //console.log(nodesList[i].adjList)
-                return nodesList[i].adjList
+    depthSearch(startPosition, endPosition, queue = [startPosition.toString()], visited = []) {
+        const pathMap = new Map();
+        appendPath(pathMap, startPosition.toString(), startPosition.toString());
+        let thisPath;
+        let lastPosition;
+        let originalPosition = startPosition;
+        //console.log(pathMap);
+        while (queue.length>0) {
+            if (startPosition.toString() === endPosition.toString()) {
+                thisPath = pathMap.get(startPosition.toString());
+                return console.log (`Shortest route from ${originalPosition} to ${endPosition} is via ${thisPath}`);
             }
             else {
-            
+                let moveAdj = this.adjacencyList.get(startPosition.toString());
+                visited.push(startPosition.toString());
+                queue.shift(1);
+                
+                for (let m = 0; m<moveAdj.length; m++) {
+                    thisPath = pathMap.get(startPosition.toString());
+                    thisPath = `${thisPath} -> ${moveAdj[m]}`;
+                    (visited.includes(moveAdj[m]) || queue.includes(moveAdj[m])) ? '' : addPathway(queue, moveAdj[m], pathMap, thisPath);
+                    
+                }      
             }
+            lastPosition = startPosition;
+            //console.log(lastPosition.toString());
+            startPosition = queue[0];
+            //console.log(pathMap.get(lastPosition));
 
+            
+            
+            //console.log(pathMap)
         }
 
-        
     }
-}
-
-class node {
-    constructor(position, nodesList) {
-        this.position = position;
-        this.adjList = buildAdjList(position, nodesList);
-    }
-
 }
 
 function buildBoard(top, side, array = []) {
@@ -70,14 +55,20 @@ function buildBoard(top, side, array = []) {
             array.push([x, y])
         }
     }
-    
-    //console.log(array);
+
     return array;
 }
 
-function buildAdjList(position, nodesList, adjList=[]) {
-    let x = position[0]
-    let y = position[1]
+
+function buildAdjArray(board, adjArray=[]) {
+    for (i in board) {
+        adjArray.push(buildAdjList(board[i]));
+    }
+    return adjArray;
+    
+}
+
+function buildAdjList(position, adjList=[]) {
     let moves = []
     moves.push([position[0]+2, position[1]+1],[position[0]+1, position[1]+2],[position[0]-1, position[1]+2], [position[0]-2, position[1]+1], [position[0]-2, position[1]-1],[position[0]-1, position[1]-2], [position[0]+1, position[1]-2],[position[0]+2, position[1]-1])
     for (i in moves) {
@@ -91,46 +82,58 @@ function buildAdjList(position, nodesList, adjList=[]) {
     return adjList;
 }
 
-function knightMoves(adjacent, prevPosition, endPosition, moveCounter, moves, ) {
-    
-    for (i in adjacent) {
-        currentPosition = adjacent[i];
-        console.log(currentPosition); 
-        console.log(prevPosition);  
-        console.log(adjacent);
-        if ((adjacent[i][0] === endPosition[0] && adjacent[i][1] === endPosition[1] )) {
-            moveCounter++;
-            console.log(`Found it in ${moveCounter} moves`)
-            return moves.push(moveCounter);
-        }
-        else {
-            let adjList = gameBoard.getAdjacent(gameBoard.nodesList, adjacent[i])
-            for (i in adjList) {
-                if (adjList[i][0] === prevPosition[0] && adjList[i][1] === prevPosition[1]){
-                    adjList.splice(i, 1);
-                }
-            }
-            prevPosition = currentPosition;
-            moveCounter++;
-            return knightMoves(adjList, prevPosition, endPosition, moveCounter, moves)
-        }
-        
-    }
+function appendPath(pathMap, vortex, thisPath) {
+    pathMap.set(vortex, thisPath);
 }
 
-const gameBoard = new board();
-
-gameBoard.setNodes();
-
-gameBoard.addVertex(gameBoard.nodesList);
-
-gameBoard.addEdges();
+function addPathway(queue, moveAdj, pathMap, thisPath) {
+    queue.push(moveAdj);
+    appendPath(pathMap, moveAdj, thisPath);
+}
 
 
-//console.log(gameBoard.nodesList);
 
-//console.log(gameBoard.nodesList[27])
 
-//gameBoard.moveKnight([2,3],[3,2])
+let game = new gameBoard();
+const board = buildBoard(8,8);
 
-//gameBoard.moveKnight(gameBoard.nodesList, [3,3], [6,6])
+
+for (i in board) {
+    game.addVertex(board[i].toString());
+}
+
+//console.log(board);
+
+/* for (i in board) {
+    
+    let adj = buildAdjList(board[i]);
+    //console.log(adj);
+    return adj;
+    //
+       
+} */
+
+const adj = buildAdjArray(board);
+
+for (a in board) {   
+    //console.log(board[a], adj[a]);
+        for (b in adj[a]) {
+            game.addEdges(board[a], adj[a][b]);
+        }
+        
+    
+}
+
+game.depthSearch([0,1],[7,7])
+
+//console.log(game.adjacencyList);
+
+/* let test = new gameBoard;
+test.addVertex(2);
+test.addVertex(3);
+test.addVertex(1);
+test.addEdges(2,2);
+test.addEdges(2,1);
+test.addEdges(2,3);
+
+console.log(test.adjacencyList); */
